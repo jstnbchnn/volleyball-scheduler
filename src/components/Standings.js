9d48335cfa9629adbd82c612f3315656dedf4438
Justin Buchanan <jstnbchnn@gmail.com>
2018-07-10 15:25:53 -0400

    Beginning of standings implementation

--- src/components/Standings.js
+++ src/components/Standings.js
@@ -1,25 +1,41 @@
 import React, { Component, Fragment } from 'react';
+import { Transition, animated } from 'react-spring';
 import styled from 'styled-components';
 
 class Standings extends Component {
 
   renderStandings = () => {
-    return this.props.standings.map(team => {
-      return <li>{team.teamName}</li>
+    return this.props.standings.map((team, idx) => styles => {
+      return (
+        <animated.li key={`${team.teamName}|${idx}`} style={{ ...defaultStyles, ...styles }}>
+          {team.teamName}
+        </animated.li>
+      )
     })
   }
 
   render () {
-    console.log(['this.props', this.props.standings]);
     return (
       <Fragment>
         <Title>{`Standings for ${this.props.selectedDivision}`}</Title>
         <ol>
-          {this.renderStandings()}
+          <Transition
+            native
+            keys={this.props.standings.map(team => team.teamName)}
+            from={{ opacity: 0, height: 0 }}
+            enter={{ opacity: 1, height: 30 }}
+            leave={{ opacity: 0 }}
+          >
+            {this.renderStandings()}
+          </Transition>
         </ol>
       </Fragment>
     )
   }
+}
+
+const defaultStyles = {
+  margin: '10px',
 }
 
 const Title = styled.span`
