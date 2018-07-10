591a472aac58ea4372d658c93e2017e8a1463548
Justin Buchanan <jstnbchnn@gmail.com>
2018-07-10 15:23:43 -0400

    Creates a custom select component

---
+++ src/components/Select.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+import ReactSelect from 'react-select';
+import 'react-select/dist/react-select.css';
+
+const Select = ({ name, onChange, options, value, disabled, label }) => (
+  <label for={name}>
+    {label}
+    <WrappedSelect
+      name={name}
+      value={value}
+      onChange={onChange}
+      options={options}
+      disabled={disabled}
+    />
+  </label>
+)
+
+const WrappedSelect = styled(ReactSelect)`
+  margin-top: 5px;
+  margin-bottom: 15px;
+  width: 350px;
+`
+
+export default Select
